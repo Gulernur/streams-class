19b76b646e6df253c9c9d045b88bb191d8f08d49
gulernurdonmez <gule0153@stud.kea.dk>
2022-09-14 11:17:21 +0200

    added comments

--- src/Ex1.java
+++ src/Ex1.java
@@ -8,24 +8,26 @@ public class Ex1 {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("peter","jan","john","janne","celine");
 
-        //1
+        //1. Create or print a list with the same size, but with each name capitalised (Peter, Jan ...)
         names.stream()
                 .map(name -> name.substring(0,1).toUpperCase()+name.substring(1)).sorted(Comparator.reverseOrder());
         //      .forEach(System.out::println);
 
-        //2
+        //2. Change the list above so that all items are sorted alphabetically
         names.stream()
                 .map(name -> name.substring(0,1).toUpperCase()+name.substring(1))
                 .sorted();
         //      .forEach(System.out::println);
 
-        //3
+        //3. Change the list above so that all items are sorted in the reverse order
         names.stream()
                 .map(name -> name.substring(0,1).toUpperCase()+name.substring(1))
                 .sorted(Comparator.reverseOrder());
         //      .forEach(System.out::println);
 
-        //4
+         /* 4. Change the example above to return a single string, like this:
+         "Sorted names as String : Peter,John,Janne,Jan,Celine"
+         */
         String result = "Sorted names as String: " + names.stream()
                 .map(name -> name.substring(0,1).toUpperCase()+name.substring(1))
                 .sorted()
